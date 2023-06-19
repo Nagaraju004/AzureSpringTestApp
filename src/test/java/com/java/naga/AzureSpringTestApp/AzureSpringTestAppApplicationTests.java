0ddc0c9fd6e8be8d9c658d4abcdbe867a188ac8f
Saravanan <nsarav756@apac.comcast.com>
2023-06-19 20:31:53 +0530

    first commit

---
+++ src/test/java/com/java/naga/AzureSpringTestApp/AzureSpringTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.naga.AzureSpringTestApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureSpringTestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
