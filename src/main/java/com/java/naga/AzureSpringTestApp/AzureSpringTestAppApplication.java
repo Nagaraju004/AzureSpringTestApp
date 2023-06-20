ee01dded2d184f13cc9514a8a9e59021518a85c0
Saravanan <nsarav756@apac.comcast.com>
2023-06-20 12:53:17 +0530

    updated main file with message

--- src/main/java/com/java/naga/AzureSpringTestApp/AzureSpringTestAppApplication.java
+++ src/main/java/com/java/naga/AzureSpringTestApp/AzureSpringTestAppApplication.java
@@ -11,7 +11,7 @@ public class AzureSpringTestAppApplication {
 
 	@GetMapping("/message")
 	public String message() {
-		return "Congrats ! your application deployed successfully in Azure Platform. !. Updated message";
+		return "Congrats ! your application deployed successfully in Azure Platform. !. Updated message is here";
 	}
 
 	public static void main(String[] args) {
