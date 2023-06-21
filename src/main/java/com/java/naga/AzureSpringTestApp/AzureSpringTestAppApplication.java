ec79e0c214c484bf73a615ac86c12b845c76e5b2
Saravanan <nsarav756@apac.comcast.com>
2023-06-21 12:23:30 +0530

    updated wish msg

--- src/main/java/com/java/naga/AzureSpringTestApp/AzureSpringTestAppApplication.java
+++ src/main/java/com/java/naga/AzureSpringTestApp/AzureSpringTestAppApplication.java
@@ -11,7 +11,7 @@ public class AzureSpringTestAppApplication {
 
 	@GetMapping("/message")
 	public String message() {
-		return "Congrats ! your application deployed successfully in Azure Platform. !. Updated message is here";
+		return "Advance happy birthday Bablooo :)";
 	}
 
 	public static void main(String[] args) {
